cc119f421e42c67abb5629d718ca38f441d002a8
Michael Truong <michaeltruong@Michaels-MacBook-Pro.local>
2021-01-14 20:01:17 -0500

    Commit 9: Reset database and added wiki descriptions

--- matrix_app_api/db/seeds.rb
+++ matrix_app_api/db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Character.create([
-    { name: "Neo", description: false },
-	{ name: "Trinity", description: false },
-	{ name: "Morpheus", description: false },
-	{ name: "Agent Smith", description: false },
+    { name: "Neo", description: "Neo (born as Thomas A. Anderson, also known as The One, an anagram for Neo) is a fictional character and the protagonist of the Matrix franchise." },
+	{ name: "Trinity", description: "Morpheus is a fictional character in The Matrix franchise.[1] He is portrayed by Laurence Fishburne in the films, and in the video game The Matrix: Path of Neo where he was the only actor to reprise his character's voice." },
+	{ name: "Morpheus", description: "Trinity is a fictional character in the Matrix franchise.[1] She is portrayed by Carrie-Anne Moss in the films. In the gameplay segments of Path of Neo, she is voiced by Jennifer Hale. Trinity first appears in the first film in the trilogy, The Matrix." },
+	{ name: "Agent Smith", description: "Agent Smith is a fictional character and the main antagonist of The Matrix trilogy. He was portrayed by Hugo Weaving in the films and voiced by Christopher Corey Smith in The Matrix: Path of Neo." },
 ])
